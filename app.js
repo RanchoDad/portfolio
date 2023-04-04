fc1c084c170368e718db450e2d16eb02a5143faf
RanchoDad <joshuagarber215@gmail.com>
2023-04-04 11:34:32 -0700

    updated js style and index

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const modal = document.getElementById("tech");
+const modal2 = document.getElementById("travel");
 const clickTechDeets = document.getElementById("modal1");
+const clickTravelDeets = document.getElementById("modal2");
 const overlay1 = document.getElementById("overlay1")
 const overlay2 = document.getElementById("overlay2")
+const closeButton = document.querySelector('.closebutton');
+const closeButton2 = document.querySelector('.closebutton2');
+
 
 clickTechDeets.addEventListener("click", function(){
     modal.style.display = "block";
     overlay1.style.display ="block";
 })
 
-let modal2 = document.getElementById("travel");
-let clickTravelDeets = document.getElementById("modal2");
 clickTravelDeets.addEventListener("click", function(){
     modal2.style.display = "block";
     overlay2.style.display ="block";
 
 })
 overlay1.addEventListener("click", function(e) {
-    if (e.target == overlay1) {
+    if (e.target === overlay1) {
       modal.style.display = "none";
       overlay1.style.display ="none";
     }
-  });
-  overlay2.addEventListener("click", function(e) {
-    if (e.target == overlay2) {
+});
+overlay2.addEventListener("click", function(e) {
+    if (e.target === overlay2) {
       modal2.style.display = "none";
       overlay2.style.display ="none";
     }
-  });
+});  
+closeButton.addEventListener("click", function(e){
+    if (e.target === closeButton){
+        modal.style.display = "none";
+        overlay1.style.display = "none";
+    }
+});  
+
+closeButton2.addEventListener("click", function(e){
+    if (e.target === closeButton2){
+        modal2.style.display = "none";
+        overlay2.style.display = "none";
+    }
+});  
+
