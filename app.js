c44ed5a62c8abde9ab1dde82a7bf86660aca9910
RanchoDad <joshuagarber215@gmail.com>
2023-04-03 20:29:40 -0700

    added modal, updated style

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-let modal = document.getElementById("tech");
-let clickTechDeets = document.getElementById("modal1");
+const modal = document.getElementById("tech");
+const clickTechDeets = document.getElementById("modal1");
+const overlay1 = document.getElementById("overlay1")
+const overlay2 = document.getElementById("overlay2")
+
 clickTechDeets.addEventListener("click", function(){
-    modal.style.display = "block"
+    modal.style.display = "block";
+    overlay1.style.display ="block";
 })
 
 let modal2 = document.getElementById("travel");
 let clickTravelDeets = document.getElementById("modal2");
 clickTravelDeets.addEventListener("click", function(){
-    modal2.style.display = "block"
-})
+    modal2.style.display = "block";
+    overlay2.style.display ="block";
 
-window.addEventListener("click", function(e) {
-    if (e.target == modal) {
+})
+overlay1.addEventListener("click", function(e) {
+    if (e.target == overlay1) {
       modal.style.display = "none";
+      overlay1.style.display ="none";
     }
   });
-
-  window.addEventListener("click", function(e) {
-    if (e.target == modal2) {
+  overlay2.addEventListener("click", function(e) {
+    if (e.target == overlay2) {
       modal2.style.display = "none";
+      overlay2.style.display ="none";
     }
   });
